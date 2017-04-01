24c13915c7d871122abc6866a64bc415a94f2122
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-04-01 17:51:25 -0400

    Adds a silly courses index page

---
+++ app/models/course.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  city: DS.attr('string'),
+  state: DS.attr('string'),
+  country: DS.attr('string'),
+  name: DS.attr('string'),
+  holeCount: DS.attr('number')
+});
