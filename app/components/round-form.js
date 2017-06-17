74c5f8a332951383c6881dff03c6cd683e9a99e5
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-06-17 11:44:33 -0400

    Adds ability to add users to round
    * We still need to make sure we can’t search for duplicates

--- app/components/round-form.js
+++ app/components/round-form.js
@@ -3,20 +3,35 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   locatedCourses: null,
   model: null,
-  searchedCourse: null,
+  searchedCourses: null,
+  searchedUsers: null,
+  userFilter: null,
 
   actions: {
     addCourse(course) {
       this.get('model').set('course', course);
     },
 
-    cancelCourse() {
+    addUser(user) {
+      this.setProperties({
+        searchedUsers: null,
+        userFilter: null
+      });
+
+      this.get('model.users').addObject(user);
+    },
+
+    removeCourse() {
       this.setProperties({
         locatedCourses: null,
         searchedCourses: null
       });
 
       this.get('model').set('course', null);
+    },
+
+    removeUser(user) {
+      this.get('model.users').removeObject(user);
     }
   }
 });
--- app/models/round.js
+++ app/models/round.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
   createdAt: DS.attr('date'),
 
   course: DS.belongsTo('course'),
-  scorecards: DS.hasMany('scorecard')
+  scorecards: DS.hasMany('scorecard'),
+
+  // Used for round-form
+  users: DS.hasMany('user')
 });
