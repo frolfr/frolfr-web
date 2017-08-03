2ef45664b18f0547d260c6947f0225db36e8bd20
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-08-03 12:01:02 -0400

    Adds some basic transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,2 +1,50 @@
 export default function(){
+  this.transition(
+    this.fromRoute('courses'),
+    this.toRoute('users'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('courses'),
+    this.toRoute('rounds'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('users'),
+    this.toRoute('rounds'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('rounds'),
+    this.toRoute('round'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('courses'),
+    this.toRoute('course'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('scorecards'),
+    this.toRoute('round'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('round'),
+    this.toRoute('turns'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
 }
