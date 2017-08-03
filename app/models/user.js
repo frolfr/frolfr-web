1d6994907ed175fa51b0241938e783a70040a9f1
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-08-03 15:15:41 -0400

    Cleans up some constants in models/user

--- app/models/user.js
+++ app/models/user.js
@@ -1,31 +1,33 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+const { attr, hasMany, Model } = DS;
+const { computed } = Ember;
 const DEFAULT_AVATAR = 'http://res.cloudinary.com/frolfr/image/upload/v1450881126/avatar-placeholder_n7w7z8.png';
 
-export default DS.Model.extend({
-  avatarUrl: DS.attr('string'),
-  email: DS.attr('string'),
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  middleName: DS.attr('string'),
-  password: DS.attr('string'),
+export default Model.extend({
+  avatarUrl: attr('string'),
+  email: attr('string'),
+  firstName: attr('string'),
+  lastName: attr('string'),
+  middleName: attr('string'),
+  password: attr('string'),
 
-  scorecards: DS.hasMany('scorecards'),
+  scorecards: hasMany('scorecards'),
 
-  name: Ember.computed('_fullNames', function() {
+  name: computed('_fullNames', function() {
     return this.get('_fullNames').join(' ');
   }),
 
-  initials: Ember.computed('_fullNames', function() {
+  initials: computed('_fullNames', function() {
     return this.get('_fullNames').map(function(name) { return name[0]; }).join('');
   }),
 
-  avatarSrc: Ember.computed('avatarUrl', function() {
+  avatarSrc: computed('avatarUrl', function() {
     return this.get('avatarUrl') || DEFAULT_AVATAR;
   }),
 
-  _fullNames: Ember.computed('firstName', 'middleName', 'lastName', function() {
+  _fullNames: computed('firstName', 'middleName', 'lastName', function() {
     return [
       this.get('firstName'),
       this.get('middleName'),
