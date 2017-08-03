e9aa5b67ef4d571732e82008692f7468f22e24bf
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-08-03 15:14:19 -0400

    Defaults to an anonymous avatar picture

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+const DEFAULT_AVATAR = 'http://res.cloudinary.com/frolfr/image/upload/v1450881126/avatar-placeholder_n7w7z8.png';
+
 export default DS.Model.extend({
   avatarUrl: DS.attr('string'),
   email: DS.attr('string'),
@@ -17,6 +19,10 @@ export default DS.Model.extend({
 
   initials: Ember.computed('_fullNames', function() {
     return this.get('_fullNames').map(function(name) { return name[0]; }).join('');
+  }),
+
+  avatarSrc: Ember.computed('avatarUrl', function() {
+    return this.get('avatarUrl') || DEFAULT_AVATAR;
   }),
 
   _fullNames: Ember.computed('firstName', 'middleName', 'lastName', function() {
