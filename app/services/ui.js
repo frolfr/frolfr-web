fd722c235c559badbb1fd3f8546f57a84fe27db0
Chris McCuller <cmcculler@salesforce.com>
2017-08-23 21:07:17 -0400

    update disabled routes to include round.turn

--- app/services/ui.js
+++ app/services/ui.js
@@ -6,13 +6,13 @@ export default Service.extend({
   router: service(),
 
   isFabVisible: computed('router.currentRouteName', function() {
-    const disabledRoutes = [ 'rounds.new', 'rounds.current' ];
+    const disabledRoutes = [ 'rounds.new', 'round.turns' ];
     const currentRouteName = this.get('router.currentRouteName');
     return !disabledRoutes.includes(currentRouteName);
   }),
 
   isBottomNavVisible: computed('router.currentRouteName', function() {
-    const disabledRoutes = [ 'rounds.new', 'rounds.current' ];
+    const disabledRoutes = [ 'rounds.new', 'round.turns' ];
     const currentRouteName = this.get('router.currentRouteName');
     return !disabledRoutes.includes(currentRouteName);
   })
