111be89fe3c51197d1a3c98a9de623b121014356
Chris McCuller <cmcculler@salesforce.com>
2017-09-04 14:43:57 -0400

    upgrade :allthethings: to 2.15

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     'ember-bootstrap': {
       'bootstrapVersion': 3,
       'importBootstrapFont': true,
