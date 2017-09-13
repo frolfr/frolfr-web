5707de6e198242d9c15354eb20418f650a593148
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-09-13 19:43:22 -0400

    Adds authenticator/authorizor and passes to adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:oauth2',
   host: ENV.apiHost,
   namespace: 'jsonapi'
 });
