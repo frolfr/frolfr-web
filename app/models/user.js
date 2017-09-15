b11078ebad9b5ac09774a8d2b5216c48acdd5fcd
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-09-15 13:41:20 -0400

    Refactors template logic into user-avatar component
    This kinda messes up the round resource, but will update soon

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 
 const { attr, hasMany, Model } = DS;
 const { computed } = Ember;
-const DEFAULT_AVATAR = 'http://res.cloudinary.com/frolfr/image/upload/v1450881126/avatar-placeholder_n7w7z8.png';
 
 export default Model.extend({
   avatarUrl: attr('string'),
@@ -21,10 +20,6 @@ export default Model.extend({
 
   initials: computed('_fullNames', function() {
     return this.get('_fullNames').map(function(name) { return name[0]; }).join('');
-  }),
-
-  avatarSrc: computed('avatarUrl', function() {
-    return this.get('avatarUrl') || DEFAULT_AVATAR;
   }),
 
   _fullNames: computed('firstName', 'middleName', 'lastName', function() {
