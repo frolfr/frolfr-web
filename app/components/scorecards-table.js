5a77c71917b79376e1640299c9c3d77a4b93c6d0
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-09-15 13:03:54 -0400

    Initial pass at re-styling the scorecards table

--- app/components/scorecards-table.js
+++ app/components/scorecards-table.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: [ 'table-responsive' ],
+  classNames: [ 'table-wrapper' ],
   course: null,
   round: null,
   scorecards: null,
