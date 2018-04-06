edfa3e696f5fefeed77f3d3914d9d76e87ab2eaa
Geoffrey Schorkopf <gschorkopf@gmail.com>
2018-04-06 17:03:37 -0400

    Paginates scorecards

---
+++ app/controllers/user/scorecards.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  page: 1,
+  queryParams: ['page', 'size'],
+  size: 5,
+
+  count: Ember.computed('model.meta.pagination.last.number', 'model.meta.pagination.self.number', function() {
+    const total = this.get('model.meta.pagination.last.number') || this.get('model.meta.pagination.self.number');
+    if (!total) return [];
+
+    return new Array(total+1).join('x').split('').map((e,i) => i+1);
+  }),
+
+  actions: {
+    goToRound(round) {
+      this.transitionToRoute('round', round);
+    }
+  }
+});
---
+++ app/routes/user/scorecards.js
@@ -0,0 +1,36 @@
+import ApplicationRoute from 'frolfr-web/routes/application';
+import Ember from 'ember';
+
+export default ApplicationRoute.extend({
+  model(params) {
+    return this.store.query('round', {
+      page: {
+        number: params.page,
+        size: params.size
+      },
+      filter: {
+        userId: this.modelFor('user').get('id')
+      },
+      include: 'course,scorecards,scorecards.turns,users'
+    });
+  },
+
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    }
+  },
+
+  afterModel() {
+    Ember.$('html, body').animate({ scrollTop: 0 });
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+
+    controller.set('userId', this.modelFor('user').get('id'));
+  }
+});
