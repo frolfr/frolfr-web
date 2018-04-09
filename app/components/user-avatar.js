8ac995d9b4291773c4b32e49c870371f9a34c2b4
Geoffrey Schorkopf <gschorkopf@gmail.com>
2018-04-09 19:02:26 -0400

    Ensures images are https urls

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 const { Component, computed } = Ember;
-const defaultAvatar = 'http://res.cloudinary.com/frolfr/image/upload/v1450881126/avatar-placeholder_n7w7z8.png';
+const defaultAvatar = 'https://res.cloudinary.com/frolfr/image/upload/v1450881126/avatar-placeholder_n7w7z8.png';
 
 export default Component.extend({
   attributeBindings: [ 'alt', 'src', 'title' ],
