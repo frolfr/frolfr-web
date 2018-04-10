c71ea79402d79198d76550e31355741616eceb80
Geoffrey Schorkopf <gschorkopf@gmail.com>
2018-04-10 15:12:00 -0400

    Paginates the course scorecards section

--- app/routes/course/scorecards.js
+++ app/routes/course/scorecards.js
@@ -1,7 +1,36 @@
 import ApplicationRoute from 'frolfr-web/routes/application';
+import Ember from 'ember';
 
 export default ApplicationRoute.extend({
-  model() {
-    return this.modelFor('course').get('rounds');
+  model(params) {
+    return this.store.query('round', {
+      page: {
+        number: params.page,
+        size: params.size
+      },
+      filter: {
+        courseId: this.modelFor('course').get('id')
+      },
+      include: 'scorecards,scorecards.turns,scorecards.user'
+    });
+  },
+
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    }
+  },
+
+  afterModel() {
+    Ember.$('html, body').animate({ scrollTop: 0 });
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+
+    controller.set('courseId', this.modelFor('course').get('id'));
   }
 });
